b715301fc2baac09494e88e9eadda43c8d517c48
TranHoangAnhDung <thad091099@gmail.com>
2024-11-03 19:42:49 +0700

    lesson-02

--- Lesson/lesson02/readme.md
+++ Lesson/lesson02/readme.md
@@ -2,3 +2,10 @@ localhost/products?minPrice=?&maxPrice?
 localhost/customer/:id
 - Param động là request param
 - phần sau dấu ? đc gọi là query Param
+
+// get query string from url
+req.query
+// get request param
+req.params
+// method post 
+req.body
--- Lesson/lesson02/server/index.js
+++ Lesson/lesson02/server/index.js
@@ -1,13 +1,85 @@
 import express from "express";
+import { customers, products, orders } from "./data.js";
+
 const port = 8080;
 // khởi tạo server
 const app = express();
-//nhận vào 1 path và 1 cb fc
-app.get("", (req, res) => {
-  const data = { school: "mindX" };
-  res.send(data);
+
+app.use(express.json());
+
+//get
+//nhận vào 1 path và 1 cb fc(request,response,next)
+app.get("/customers/:id", (req, res) => {
+  const { id } = req.params;
+  const customer = customers.find((customer) => customer.id === id);
+  //check customer
+  if (customer) {
+    res.status(200).send({
+      status: "success",
+      message: "data found",
+      data: customer,
+    });
+  } else {
+    res.status(400).send({
+      status: "fail",
+      message: "data not found",
+      data: null,
+    });
+  }
 });
 
+// get
+// lấy others dựa trên customerId và productId
+app.get("/others/:cId/:pId", (req, res) => {
+  const { cId, pId } = req.params;
+  const listOther = orders.filter(
+    (order) => order.customerId === cId && order.productId === pId
+  );
+  if (listOther) {
+    res.status(200).send({
+      status: "success",
+      message: "data found",
+      data: listOther,
+    });
+  } else {
+    res.status(404).send({
+      status: "fail",
+      message: "data not found",
+      data: null,
+    });
+  }
+  res.send(listOther);
+});
+
+// Post
+// thêm 1 customer mới
+app.post("/customers", (req, res) => {
+  const body = req.body;
+  customers.push(body);
+  res.send(customers);
+});
+
+//Update
+app.put("customers/:id", (req, res) => {
+  const dataUpdate = req.body;
+  const { id } = req.params;
+
+  const customer = customers.find((customer) => customer.id === id);
+  Object.keys(dataUpdate).forEach((key) => {
+    console.log(key);
+    customer[key] = dataUpdate[key];
+  });
+  res.send(customer);
+});
+
+// Delete = Delete
+app.delete("/customers/:id", (req, res) => {
+  const { id } = req.params;
+  const filterCustomers = customers.filter((c) => c.id != id);
+  res.send(filterCustomers);
+});
+
+//run server
 app.listen(port, () => {
   console.log("Server is running!");
 });
